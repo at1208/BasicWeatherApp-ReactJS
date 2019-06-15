728dd107b7534671a71d391e0b9dbd4686bcc364
aman tiwari <amantiwari12081999@gmail.com>
2019-06-15 17:45:14 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,4 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
